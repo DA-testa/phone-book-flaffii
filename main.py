6fcbe75d195bfe0f9b71cd4697bc6574b4993c4e
Niks Bugnēvics <70239899+flaffii@users.noreply.github.com>
2023-04-03 01:17:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 # python3
-
 class Query:
     def __init__(self, query):
         self.type = query[0]
         self.number = int(query[1])
         if self.type == 'add':
             self.name = query[2]
-
 def read_queries():
     n = int(input())
     return [Query(input().split()) for i in range(n)]
@@ -21,11 +19,11 @@ def process_queries(queries):
         if cur_query.type == 'add':
             contacts[cur_query.number] = cur_query.name
         elif cur_query.type == 'del':
-            contacts.pop(cur_query.number, None)
+            if cur_query.number in contacts:
+                del contacts[cur_query.number]
         else:
-            name = contacts.get(cur_query.number, 'not found')
-            result.append(name)
+            response = contacts.get(cur_query.number, 'not found')
+            result.append(response)
     return result
-
 if __name__ == '__main__':
     write_responses(process_queries(read_queries()))
